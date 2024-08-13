c36fb2ff634144746939a9b4ff17be1b04d2266a
HalimLuman <halim.luman0@gmail.com>
2024-08-13 08:52:14 +0200

    implement auth, appwrite, and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,17 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
 
-  const loggedIn = { firstName: "Halim", lastName: "Luman", email: "halim.luman0@gmail.com"};
+  const loggedIn = await getLoggedInUser();
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || "Guest"} subtext = "Access and manage your account and transactions efficiently"/>
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.name || "Guest"} subtext = "Access and manage your account and transactions efficiently"/>
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.45}/>
         </header>
       </div>
